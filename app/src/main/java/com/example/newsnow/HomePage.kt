5ad053d4a60d6f905a86d271e1c23bc51ca03e48
HarunRashid-dev <harunrashid9159@gmail.com>
2025-02-26 10:26:49 +0530

    Code Completed till 13:42

--- app/src/main/java/com/example/newsnow/HomePage.kt
+++ app/src/main/java/com/example/newsnow/HomePage.kt
@@ -2,9 +2,11 @@ package com.example.newsnow
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -13,8 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.kwabenaberko.newsapilib.models.Article
 
 @Composable
@@ -48,7 +52,13 @@ fun ArticleItem(article: Article){
                 .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ){
-
+            AsyncImage(
+                model = article.urlToImage,
+                contentDescription = "Article image",
+                modifier = Modifier.size(80.dp)
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
+            )
         }
     }
 }
